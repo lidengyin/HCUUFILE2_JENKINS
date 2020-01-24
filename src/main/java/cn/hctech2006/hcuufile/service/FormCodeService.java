b3f819ba232ee1a7594da1856f1a53cdf4b41699
lidengyin <2743853037@qq.com>
2020-01-24 10:57:35 +0800

    初始化

---
+++ src/main/java/cn/hctech2006/hcuufile/service/FormCodeService.java
@@ -0,0 +1,16 @@
+package cn.hctech2006.hcuufile.service;
+
+import cn.hctech2006.hcuufile.bean.HcUuFormCode;
+import cn.hctech2006.hcuufile.mapper.HcUuFormCodeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FormCodeService {
+    @Autowired
+    private HcUuFormCodeMapper hcUuFormCodeMapper;
+    public HcUuFormCode findByRecordHex(String codeHex){
+        return hcUuFormCodeMapper.selectByCodeHex(codeHex);
+    }
+
+}
